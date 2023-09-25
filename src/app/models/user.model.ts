bb2466c5dedc3d4ab5ac28aa75e249f2cfd62553
Jonathan Grimm <jonathan.grimm@upshop.com>
2023-09-24 22:52:24 -0500

    formatted

--- src/app/models/user.model.ts
+++ src/app/models/user.model.ts
@@ -1,32 +1,43 @@
 // user.model.ts
-import { randJobArea, randJobTitle, randJobDescriptor, randCompanyName, randPhoneNumber, randAddress, Address, randAmount, randCatchPhrase, randSubscriptionPlan } from '@ngneat/falso';
+import {
+  randJobArea,
+  randJobTitle,
+  randJobDescriptor,
+  randCompanyName,
+  randPhoneNumber,
+  randAddress,
+  Address,
+  randAmount,
+  randCatchPhrase,
+  randSubscriptionPlan,
+} from '@ngneat/falso';
 
 export class User {
-    id: number;
-    name: string;
-    email: string;
-    imageUrl: string;
-    phoneNumber: string;
-    jobTitle: string;
-    company: string;
-    jobDescription: string;
-    address: Address;
-    salesRevenue: string;
-    catchPhrase: string;
-    subscriptionPlan: string;
+  id: number;
+  name: string;
+  email: string;
+  imageUrl: string;
+  phoneNumber: string;
+  jobTitle: string;
+  company: string;
+  jobDescription: string;
+  address: Address;
+  salesRevenue: string;
+  catchPhrase: string;
+  subscriptionPlan: string;
 
-    constructor(data: any) {
-        this.id = data.id;
-        this.name = data.name;
-        this.email = data.email;
-        this.imageUrl = data.imageUrl;
-        this.phoneNumber =  randPhoneNumber({countryCode:'US'}),
-        this.jobDescription = randJobDescriptor(),
-        this.jobTitle = randJobTitle(),
-        this.company = randCompanyName(),
-        this.address = randAddress({includeCounty: false}),
-        this.salesRevenue = randAmount({min: 1000, symbol: '$'}),
-        this.catchPhrase = randCatchPhrase(),
-        this.subscriptionPlan = randSubscriptionPlan()
-    }
+  constructor(data: any) {
+    this.id = data.id;
+    this.name = data.name;
+    this.email = data.email;
+    this.imageUrl = data.imageUrl;
+    (this.phoneNumber = randPhoneNumber({ countryCode: 'US' })),
+      (this.jobDescription = randJobDescriptor()),
+      (this.jobTitle = randJobTitle()),
+      (this.company = randCompanyName()),
+      (this.address = randAddress({ includeCounty: false })),
+      (this.salesRevenue = randAmount({ min: 1000, symbol: '$' })),
+      (this.catchPhrase = randCatchPhrase()),
+      (this.subscriptionPlan = randSubscriptionPlan());
+  }
 }
--- src/app/user-list/user-list.component.ts
+++ src/app/user-list/user-list.component.ts
@@ -7,35 +7,37 @@ import { Router } from '@angular/router';
 @Component({
   selector: 'app-user-list',
   templateUrl: './user-list.component.html',
-  styleUrls: ['./user-list.component.css']
+  styleUrls: ['./user-list.component.css'],
 })
-
 export class UserListComponent implements OnInit {
   users: User[] = [];
   displayedColumns = ['image-name'];
 
-  constructor(private http: HttpClient, private router:Router) {}
+  constructor(private http: HttpClient, private router: Router) {}
 
   ngOnInit() {
     this.fetchUsers();
   }
 
   goToDetails(userId: number) {
-  this.router.navigate(['/user', userId]);
-}
-
-async fetchUsers() {
-  const root = await this.http.get<Root>('https://reqres.in/api/users/').toPromise();
-
-  if (root) {
-    this.users = root.data.flatMap(user => new User({
-      id:  user.id,
-      name:  `${user.first_name} ${user.last_name}`,
-      about: user.email,
-      imageUrl: user.avatar
-    }));
-  }
-}
+    this.router.navigate(['/user', userId]);
   }
 
+  async fetchUsers() {
+    const root = await this.http
+      .get<Root>('https://reqres.in/api/users/')
+      .toPromise();
 
+    if (root) {
+      this.users = root.data.flatMap(
+        (user) =>
+          new User({
+            id: user.id,
+            name: `${user.first_name} ${user.last_name}`,
+            about: user.email,
+            imageUrl: user.avatar,
+          })
+      );
+    }
+  }
+}
